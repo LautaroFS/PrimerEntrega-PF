ce6222fa86d611ac9c70a530593de0d117b347d4
LautaroGonzalez159 <lautarogonzalezsanchez1@gmail.com>
2022-12-04 20:39:08 -0300

    PrimerPaso

--- src/main.js
+++ src/main.js
@@ -13,11 +13,15 @@ app.use(express.urlencoded({ extended: true }))
 app.set('views', './views/pages')
 app.set('view engine', 'ejs')
 
-const products = []
 const messages = [{
     author: 'Robot', text: 'Hola deja tu consulta y te la contestaremos.'
 }]
-const prodCarrito = []
+const products = []
+const prodCarrito = [{
+    nombre: 'coca',
+    precio: 12,
+    img: "as"
+}]
 
 io.on('connection', socket => {
     console.log('Nuevo cliente conectado!!');
@@ -28,20 +32,57 @@ io.on('connection', socket => {
     })
 })
 
-
-app.get('/carrito', (req, res) => {
-    res.render('stock', { prodCarrito })
-})
-
+/* PRODUCTOS */
 app.get('/productos', (req, res) => {
     res.render('inicio', { products })
 })
 
-app.post('/carrito', async (req, res) => {
-    products.push(req.body)
+app.post('/productos', async (req, res) => {
+    const { nombre, precio, cantidad, img } = req.body
+    const prod = { nombre: nombre, precio: precio, cantidad: cantidad, img: img }
+
+    if (products.length === 0) {
+        prod.id = 0
+    } else {
+        const idPosterior = products[products.length - 1].id
+        prod.id = idPosterior + 1
+    }
+
+    products.push(prod)
+    console.log(products);
     res.redirect('/productos')
 })
 
+app.post('/productos', (req, res) => {
+
+})
+
+app.delete('/productos', async (req, res) => {
+    const { id } = req.params
+    const prodDelete = products.splice(parseInt(id) - 1, 1)
+    res.json({ prodDelete })
+})
+
+
+/* CARRITO */
+app.get('/carrito', (req, res) => {
+    res.render('carrito', { prodCarrito })
+})
+
+app.post('/carrito', (req, res) => {
+    const { nombre, precio, img } = req.body
+    const prod = { nombre: nombre, precio: precio, img: img }
+    prodCarrito.push(prod)
+    res.redirect('/carrito')
+})
+
+app.delete('/carrito',(req,res)=>{
+
+})
+
+
+
+/* PUESTA EN MARCHA DEL SERVER */
 httpServer.listen(PORT, () => {
     console.log(`Server corriendo en ${PORT}`);
 })
