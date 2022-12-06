af5d749b875122d3d6d194f95b5f6b99503a1d1b
LautaroGonzalez159 <lautarogonzalezsanchez1@gmail.com>
2022-12-06 18:46:58 -0300

    Tercer Prueba

--- src/main.js
+++ src/main.js
@@ -18,18 +18,6 @@ app.set('view engine', 'ejs')
 const messages = [{
     author: 'Robot', text: 'Hola deja tu consulta y te la contestaremos.'
 }]
-const products = [{
-    "id": 1,
-    "nombre": 'Coca',
-    "precio": 12,
-    "img": "https://ardiaprod.vtexassets.com/arquivos/ids/228472/Gaseosa-CocaCola-Sabor-Original-225-Lts-_2.jpg?v=637959903979400000"
-}]
-const prodCarrito = [{
-    nombre: 'Coca',
-    precio: 12,
-    img: "https://ardiaprod.vtexassets.com/arquivos/ids/228472/Gaseosa-CocaCola-Sabor-Original-225-Lts-_2.jpg?v=637959903979400000",
-    id: 0
-}]
 
 io.on('connection', socket => {
     console.log('Nuevo cliente conectado!!');
@@ -42,63 +30,6 @@ io.on('connection', socket => {
 
 app.use('/productos', routerProd)
 app.use('/carrito', routerCarrito)
-
-// app.get('/productos', (req, res) => {
-//     res.render('inicio', { products })
-// })
-
-// app.post('/productos', async (req, res) => {
-//     const { nombre, precio, cantidad, img, id } = req.body
-//     const prod = { nombre: nombre, precio: precio, cantidad: cantidad, img: img, id: id }
-
-//     if (products.length === 0) {
-//         prod.id = 0
-//     } else {
-//         const idPosterior = products[products.length - 1].id
-//         prod.id = idPosterior + 1
-//     }
-
-//     products.push(prod)
-//     console.log(products);
-//     res.redirect('/productos')
-// })
-
-
-// app.delete('/productos', async (req, res) => {
-//     const { id } = req.params
-//     const prodDelete = products.splice(parseInt(id) - 1, 1)
-//     res.redirect('/productos')
-// })
-
-
-/* CARRITO */
-// app.get('/carrito', (req, res) => {
-//     res.render('carrito', { prodCarrito })
-// })
-
-// app.post('/carrito', (req, res) => {
-//     const { nombre, precio, img, id } = req.body
-//     const prod = { nombre: nombre, precio: precio, img: img, id: id }
-
-//     if (prodCarrito.length === 0) {
-//         prod.id = 1
-//     } else {
-//         const idPosterior = prodCarrito[prodCarrito.length - 1].id
-//         prod.id = idPosterior + 1
-//     }
-
-//     prodCarrito.push(prod)
-//     console.log(prodCarrito);
-//     res.redirect('/carrito')
-// })
-
-// app.delete('/carrito', (req, res) => {
-//     const { id } = req.params
-//     const prodDelete = prodCarrito.splice(parseInt(id) + 1, 1)
-//     res.redirect('/carrito')
-// })
-
-
 
 /* PUESTA EN MARCHA DEL SERVER */
 httpServer.listen(PORT, () => {
